3b7daba1f9e84d4c459142c05f95a06e6e48a7ca
Subhankar chakrborty <subhankarchakrborty@gmail.com>
2024-04-18 10:36:23 +0530

    add update post functionlity

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,4 +1,4 @@
-import Post from "../models/post.model.js"; // Importing the Post model
+import Post from "../models/postModel.js"; // Importing the Post model
 import { errorHandler } from "../utils/error.js"; // Importing the errorHandler utility
 
 // Controller function to create a new post
@@ -90,6 +90,29 @@ export const deletepost = async (req, res, next) => {
   try {
     await Post.findByIdAndDelete(req.params.postId);
     res.status(200).json("The post has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updatepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this post"));
+  }
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        $set: {
+          title: req.body.title,
+          content: req.body.content,
+          category: req.body.category,
+          image: req.body.image,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
   } catch (error) {
     next(error);
   }

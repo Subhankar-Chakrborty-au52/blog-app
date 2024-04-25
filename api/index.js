f1cd70faad8cae316a55c83ecfecf3d2f41729d1
Subhankar chakrborty <subhankarchakrborty@gmail.com>
2024-04-25 13:42:22 +0530

    add comment section tot the post page

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 connectDB();
 
 const app = express();
@@ -27,7 +28,7 @@ app.use((err, req, res, next) => {
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
-
+app.use("/api/comment", commentRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Node server running on port ${PORT}`);
